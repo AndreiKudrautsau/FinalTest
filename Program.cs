83c5a241255fe2aaafce6486ccce068b048bf730
Andrei Kudrautsau <kavalerand@gmail.com>
2023-03-24 04:00:46 +0300

    add task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося
+// массива строк формирует массив из строк, длина которых <=3 символа. 
+// Первоначальный маcсив можно задать на старте выполнения алгоритма.
+
+string[] array = { "hello", "2", "yes", "world", ":-)" };
+Console.WriteLine();
+StartArray(array);
+Console.WriteLine();
+Console.WriteLine();
+FinishArray(array);
+Console.WriteLine();
+
+// методы
+void StartArray(string[] startArray)
+{
+    Console.Write("Start array: [");
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        if (i < startArray.Length - 1) Console.Write($"{startArray[i],8},");
+        if (i == startArray.Length - 1) Console.Write($"{startArray[i],8}]");
+        
+    }
+}
+void FinishArray(string[] startArray)
+{
+    Console.Write("Finish array: [");
+    string[] arrayNew = new string[startArray.Length];
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        if (startArray[i].Length <= 3)
+        {
+            arrayNew[i] = startArray[i];
+            if (i < startArray.Length - 1) Console.Write($"{array[i],8},");
+            if (i == startArray.Length - 1) Console.Write($"{array[i],8}]");
+        }
+    }
+    Console.WriteLine();
+}
+
